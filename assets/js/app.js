0e37920816bd525f1619927c4dd63f00abd70fbb
AndrewVakaryuk <andreyvakaryuk@gmail.com>
2020-09-18 13:19:25 +0300

    search form is added

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import 'bootstrap/scss/bootstrap.scss';
-import '../css/custom.css';
+import '../css/main.css';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
